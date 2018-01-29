4e07e64da415136062be15d181486f0f96365ae9
Vishal <vishalcseng@gmail.com>
2018-01-30 05:20:51 +0530

    Project Completed.

--- app/src/main/java/com/example/hp/magicball/MainActivity.java
+++ app/src/main/java/com/example/hp/magicball/MainActivity.java
@@ -2,12 +2,44 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button askButton = (Button) findViewById(R.id.askButton);
+        final ImageView answerImage = (ImageView) findViewById(R.id.answerImage);
+        final int[] answers=
+                {
+                        R.drawable.img0,
+                        R.drawable.img1,
+                        R.drawable.img2,
+                        R.drawable.img3,
+                        R.drawable.img4,
+                        R.drawable.img5,
+                        R.drawable.img6,
+                };
+        answerImage.setImageResource(R.drawable.home);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random rndGenegartor = new Random();
+                int Number = rndGenegartor.nextInt(8);
+                Log.d("The Random no. is" +Number);
+                answerImage.setImageResource(answers[Number]);
+
+            }
+        });
     }
 }
