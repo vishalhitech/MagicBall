48f296cd083cec9662e5d3b468fb65ee91e9ab91
Vishal <vishalcseng@gmail.com>
2018-01-31 01:50:22 +0530

    Dubugging Completed & Full Fuctioning App.

--- app/src/main/java/com/example/hp/magicball/MainActivity.java
+++ app/src/main/java/com/example/hp/magicball/MainActivity.java
@@ -15,31 +15,22 @@ import java.util.Random;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button askButton = (Button) findViewById(R.id.askButton);
-        final ImageView answerImage = (ImageView) findViewById(R.id.answerImage);
-        final int[] answers=
-                {
+        Button askButton = (Button)findViewById(R.id.askButton);
+        final ImageView answerImage = (ImageView)findViewById(R.id.answerImage);
+        final int[] answers= {
                         R.drawable.img0,
                         R.drawable.img1,
                         R.drawable.img2,
                         R.drawable.img3,
                         R.drawable.img4,
                         R.drawable.img5,
-                        R.drawable.img6,
-                };
-        answerImage.setImageResource(R.drawable.home);
-
+                        R.drawable.img6 };
         askButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Random rndGenegartor = new Random();
-                int Number = rndGenegartor.nextInt(8);
-                Log.d("The Random no. is" +Number);
-                answerImage.setImageResource(answers[Number]);
-
-            }
+                int Number = rndGenegartor.nextInt(7);
+                answerImage.setImageResource(answers[Number]);  }
         });
     }
 }
